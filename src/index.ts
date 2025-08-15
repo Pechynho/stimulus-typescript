e5bd9ffa2ffb3a852c663d5053b3779f99d98a8e
Jan Pech <pechynho@gmail.com>
2025-08-15 20:04:07 +0200

    Export `isActionEvent` utility and implement its functionality.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // Export the main components
 export { default as PortalController } from './portal-controller';
 export { Typed, Object_, ObjectAs, Target } from './typed-stimulus';
-export { camelCase, capitalize } from './utils';
+export { isActionEvent } from './utils';
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import {ActionEvent} from "@hotwired/stimulus";
+
 export const camelCase = (value: string): string => {
     return value
         .replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''))
@@ -7,3 +9,7 @@ export const camelCase = (value: string): string => {
 export const capitalize = (value: string): string => {
     return value.charAt(0).toUpperCase() + value.slice(1);
 };
+
+export const isActionEvent = (value: any): value is ActionEvent => {
+    return value instanceof Event && 'params' in value && typeof value.params !== 'object';
+}
